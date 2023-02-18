31c45008d8d863f273f1882af6694aa5433a8336
Goraki <96821952+Goraki1994@users.noreply.github.com>
2023-02-18 18:12:58 +0100

    Add files via upload

---
+++ dns_enumeration.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3 
+
+# Modules 
+import dns.resolver 
+
+# Domain and record type
+
+target_domain = 
+record_types = ["A", "AAAA", "CNAME", "MX", "NS", "SOA", "TXT"]
+
+# DNS resolver 
+resolver = dns.resolver.Resolver()
+for record_type in record_types:
+    # DNS lookup based on the specified domain and record type
+    try:
+        answers = resolver.resolve(target_domain, record_type) 
+    except dns.resolver.NoAnswer:
+        continue 
+    # Answers 
+    print(f"{record_type} records for {target_domain}:")  
+    for rdata in answers: 
+        print(f" {rdata}") 
+
